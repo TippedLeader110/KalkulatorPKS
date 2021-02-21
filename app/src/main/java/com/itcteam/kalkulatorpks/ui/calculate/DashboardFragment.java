c34ea66fd500e3fe26b837d1426ee2d2561917f5
Muhammad Bayhaqi Daulay <39187403+TippedLeader110@users.noreply.github.com>
2021-02-21 21:22:45 +0700

    Main View Calculate

--- app/src/main/java/com/itcteam/kalkulatorpks/ui/calculate/DashboardFragment.java
+++ app/src/main/java/com/itcteam/kalkulatorpks/ui/calculate/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.itcteam.kalkulatorpks.ui.calculate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +15,43 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.itcteam.kalkulatorpks.R;
+import com.itcteam.kalkulatorpks.ui.calculate.task.Calculate01;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
+    Button cal01, cal02, cal03, cal04, cal05;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+//        dashboardViewModel =
+//                ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calculate, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        cal01 = root.findViewById(R.id.cal_01);
+
+        cal01.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Calculate01.class);
+                startActivity(intent);
             }
         });
+
+        cal02 = root.findViewById(R.id.cal_02);
+        cal03 = root.findViewById(R.id.cal_03);
+        cal04 = root.findViewById(R.id.cal_04);
+        cal05 = root.findViewById(R.id.cal_05);
+
+
+//        final TextView textView = root.findViewById(R.id.text_dashboard);
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
---
+++ app/src/main/java/com/itcteam/kalkulatorpks/ui/calculate/task/Calculate01.java
@@ -0,0 +1,17 @@
+package com.itcteam.kalkulatorpks.ui.calculate.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itcteam.kalkulatorpks.R;
+
+public class Calculate01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_cal_01);
+        
+    }
+}
