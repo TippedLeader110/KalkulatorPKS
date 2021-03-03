233329fb8d2a33478479f4fc8cbeb575c7230ffc
Muhammad Bayhaqi Daulay <39187403+TippedLeader110@users.noreply.github.com>
2021-03-03 12:00:51 +0700

    carousel home fragment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KalkulatorPKS
+
+Aplikasi untuk menghitung hitungan hitungan singkat seputar PKS seperti ALB, DOBI, dll.
+
+Aplikasi ini menggunakan library git : 
+1. https://github.com/denzcoskun/ImageSlideshow
  
--- app/src/main/java/com/itcteam/kalkulatorpks/ui/home/HomeFragment.java
+++ app/src/main/java/com/itcteam/kalkulatorpks/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.itcteam.kalkulatorpks.ui.home;
 
 import android.os.Bundle;
+import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.itcteam.kalkulatorpks.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -24,6 +31,25 @@ public class HomeFragment extends Fragment {
 //                ViewModelProviders.of(this).get(HomeViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView home = root.findViewById(R.id.text_home);
+
+        String homeText = "Selamat datang \n" +
+                "Aplikasi Kalkulator PKS adalah aplikasi berbasis mobile yang memiliki fitur untuk memudahkan menghitung beberapa jenis kalkulasi yang sering di gunakan di PKS ";
+
+        home.setText(homeText);
+
+        ArrayList<SlideModel> imageList = new ArrayList<>(); // Create image list
+
+        imageList.add(new SlideModel(R.drawable.sawit01, "", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.sawit02, "", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.sawit03, "", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.sawit04, "", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.sawit05, "", ScaleTypes.CENTER_CROP));
+
+        ImageSlider imageSlider = root.findViewById(R.id.img_sawit);
+        imageSlider.setImageList(imageList);
+
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
